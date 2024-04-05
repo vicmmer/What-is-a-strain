a926918f9beca316c38f5839438f32936b30c0d4
Victoria Martinez <65916041+vicmmer@users.noreply.github.com>
2024-04-05 11:59:53 -0500

    Update subsample.py

--- subsample.py
+++ subsample.py
@@ -3,10 +3,19 @@ import random
 import glob
 from subprocess import check_output
 
+#if hard-coding:
+#path_in = '/home/group6/What-is-a-strain' 
+#path_out = '/home/group6/What-is-a-strain/subsampling_output'
+
+#Soft coding, estabishing path and output folder 
 path_in = os.getcwd()
+# Define path_out as a folder named "subsampling_output" within the current directory
 path_out = os.path.join(path_in, "subsampling_output")
 
-files = glob.glob(path_in + '*')
+# Create the output directory if it doesn't exist
+os.makedirs(path_out, exist_ok=True)
+
+files = glob.glob(path_in + '/*')
 a = [i for i in files if '.fastq' in i]
 a.sort()
 
@@ -14,7 +23,7 @@ def wc(filename):
     return int(check_output(["wc", "-l", filename]).split()[0])
 
 fi = 0
-while fi < len(a):
+while fi < len(a) - 1:
     file1 = a[fi]
     file2 = a[fi+1]
 
@@ -26,10 +35,10 @@ while fi < len(a):
     lpath = len(path_in)
 
     for i in randomList:
-        command = f'seqtk sample -s{i} {file1} 150000 > {path_out}{counter}_{file1[lpath:]}'
+        command = f'seqtk sample -s{i} {file1} 150000 > {path_out}/{counter}_{os.path.basename(file1)}'
         os.system(command)
 
-        command = f'seqtk sample -s{i} {file2} 150000 > {path_out}{counter}_{file2[lpath:]}'
+        command = f'seqtk sample -s{i} {file2} 150000 > {path_out}/{counter}_{os.path.basename(file2)}'
         os.system(command)
 
         counter += 1
