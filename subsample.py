4f3b3d030474d6a4abc0a100f9a6b87d9829070a
Victoria Martinez <65916041+vicmmer@users.noreply.github.com>
2024-04-22 12:01:48 -0500

    Update subsample.py

--- subsample.py
+++ subsample.py
@@ -1,18 +1,29 @@
 import os
 import random
 import glob
-from subprocess import check_output
+from subprocess import check_output, run
+from Bio import SeqIO
 
-#if hard-coding:
-#path_in = '/home/group6/What-is-a-strain' 
-#path_out = '/home/group6/What-is-a-strain/subsampling_output'
+# Define function to count reads in a fastq file
+def count_reads(file_path):
+    with open(file_path, 'r') as file:
+        return sum(1 for line in file) // 4
 
-#Soft coding, estabishing path and output folder 
+# Define function to filter contigs longer than 1000 bp
+def filter_contigs(contigs_file_path, output_folder):
+    filtered_contigs_file = os.path.join(output_folder, 'filtered_contigs.fasta')
+    with open(contigs_file_path, 'r') as contigs_file, open(filtered_contigs_file, 'w') as filtered_file:
+        for record in SeqIO.parse(contigs_file, 'fasta'):
+            if len(record.seq) < 1000:
+                SeqIO.write(record, filtered_file, 'fasta')
+
+# For os function
 path_in = os.getcwd()
-# Define path_out as a folder named "subsampling_output" within the current directory
+
+# Define the path out as a folder called "subsampling_output" within the current directory
 path_out = os.path.join(path_in, "subsampling_output")
 
-# Create the output directory if it doesn't exist
+# Create the output directory only if it doesn't exist
 os.makedirs(path_out, exist_ok=True)
 
 files = glob.glob(path_in + '/*')
@@ -21,6 +32,8 @@ a.sort()
 
 def wc(filename):
     return int(check_output(["wc", "-l", filename]).split()[0])
+
+print(files)
 
 fi = 0
 while fi < len(a) - 1:
@@ -32,14 +45,19 @@ while fi < len(a) - 1:
     random.seed(1)
     randomList = random.sample(range(0, number_reads), 10)
     counter = 1
-    lpath = len(path_in)
 
     for i in randomList:
-        command = f'seqtk sample -s{i} {file1} 150000 > {path_out}/{counter}_{os.path.basename(file1)}'
-        os.system(command)
+        sample_name1 = os.path.basename(file1).split('_')[0]
+        sample_name2 = os.path.basename(file2).split('_')[0]
 
-        command = f'seqtk sample -s{i} {file2} 150000 > {path_out}/{counter}_{os.path.basename(file2)}'
-        os.system(command)
+        output_file1 = f"{path_out}/{sample_name1}_sub{counter}_F.fastq"
+        output_file2 =  f"{path_out}/{sample_name2}_sub{counter}_R.fastq" if file2 else None
+
+        command = f'seqtk sample -s{i} {file1} 150000 > {output_file1}'
+        run(command, shell=True, check=True)
+
+        command = f'seqtk sample -s{i} {file2} 150000 > {output_file2}'
+        run(command, shell=True, check=True)
 
         counter += 1
     fi += 2
