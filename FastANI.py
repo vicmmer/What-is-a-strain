8ee50bfd8ffbae187b653242d2cdf3b1f3409974
Victoria Martinez <65916041+vicmmer@users.noreply.github.com>
2024-04-22 12:00:11 -0500

    Update FastANI.py

--- FastANI.py
+++ FastANI.py
@@ -1,17 +1,17 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Apr 17 15:32:27 2024
+import subprocess
 
-@author: anmol
-"""
+# Replace [QUERY_GENOME], [REFERENCE_LIST], and [OUTPUT_FILE] with your actual paths
+query_genome = "./contigs_paths.txt"
+reference_list = "./contigs_paths.txt"
+output_file = "./fastani_output"
 
- #can try this first 
-import fastani
-fasta_file1 = #path to file
-fasta_file2 = #path to file
+# Construct the command as a list of strings
+command = ["fastANI", "--ql", query_genome, "--rl", reference_list, "-o", output_file]
 
-result = fastani.run(fasta_file1, fasta_file2)
+# Execute the command
+try:
+    subprocess.run(command, check=True)
+    print("Command executed successfully!")
+except subprocess.CalledProcessError as e:
+    print("Error executing command:", e)
 
-#or run this through os commands
-
-#$ ./fastANI --ql [QUERY_LIST] --rl [REFERENCE_LIST] -o [OUTPUT_FILE]
